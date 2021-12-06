acaa19edf3d396b7ce20a118f82d5c54931e4197
Artemis4777 <95307242+Artemis4777@users.noreply.github.com>
2021-12-06 10:37:21 -0500

    Update Weather_App.py

--- Weather_App.py
+++ Weather_App.py
@@ -1,9 +1,30 @@
-import requests
+import requests, json
 
 print("Welcome to the best discount weather app!")
 
 def get_zip(zip_code):
-    zip_code = input("\nPlease enter your zip code: ")
-    requests.get(f"http://api.openweathermap.org/data/2.5/forecast"
-        f"?id={zip_code}&appid=8ff4487d9dc4a13a3dc17413012d68d7")
+    response = requests.get(f"http://api.openweathermap.org/data/2.5/weather"
+        f"?q={zip_code}&appid=8ff4487d9dc4a13a3dc17413012d68d7")
+    x = response.json()
+    if x["cod"] != "404":
+        y = x["main"]
+        current_temperature = y["temp"]
+        current_temperature = (int(current_temperature) - 273.15) * 9/5 + 32
+        current_pressure = y["pressure"]
+        current_humidity = y["humidity"]
+        z = x["weather"]
+        weather_description = z[0]["description"]
+        print(" Temperature (in fahrenheit) = " +
+                    str(current_temperature) +
+          "\n atmospheric pressure (in hPa unit) = " +
+                    str(current_pressure) +
+          "\n humidity (in percentage) = " +
+                    str(current_humidity) +
+          "\n description = " +
+                    str(weather_description))
+ 
+    else:
+        print(" City Not Found ")
+
+get_zip(input("\nPlease enter your city: "))
 
