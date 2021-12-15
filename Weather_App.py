c8ee38e5de3dfd85b54a66ba26c5907c8f12ea71
Artemis4777 <95307242+Artemis4777@users.noreply.github.com>
2021-12-15 00:08:29 -0500

    Revert "Create .DS_Store"
    This reverts commit 4988ccff4f09537e198413d1495b9dff69cc2a65.

--- Weather_App.py
+++ Weather_App.py
@@ -1,14 +1,11 @@
 #import modules
-import requests, json
+import requests
 
 print("Welcome to the best discount weather app!")
 
 #main function
 def get_zip(zip_code):
     try:
-#        cnt = input("How many days would you like me to forecast? Please enter "
-#        "a number between 1 and 16: ")
-#        cnt = int(cnt)
         response = requests.get(f"http://api.openweathermap.org/data/2.5/forecast"
             f"?zip={zip_code},us&units=imperial&appid=8ff4487d9dc4a13a3dc17413012d68d7")
         x = response.json()
@@ -18,28 +15,6 @@ def get_zip(zip_code):
 
     else:
         if x["cod"] == "200":
-#            y = x["main"]
-#            current_temperature = y["temp"]
-#            current_temperature = (int(current_temperature) - 273.15) * 9/5 + 32
-#            current_pressure = y["pressure"]
-#            current_humidity = y["humidity"]
-#            z = x["weather"]
-#            weather_description = z[0]["description"]
-#            print(" Temperature (in fahrenheit) = " +
-#                    str(current_temperature) +
-#              "\n atmospheric pressure (in hPa unit) = " +
-#                    str(current_pressure) +
-#              "\n humidity (in percentage) = " +
-#                    str(current_humidity) +
-#              "\n description = " +
-#                    str(weather_description) +
-#              "\n description = " +
-#                    str(weather_description) +
-#              "\n description = " +
-#                    str(weather_description) +
-#              "\n description = " +
-#                    str(weather_description))
-#            print(x)
             d1_temp_high = x["list"][0]["main"]["temp_max"]
             d1_temp_low = x["list"][0]["main"]["temp_min"]
             d1_temp_info = x["list"][0]["weather"][0]["description"]
